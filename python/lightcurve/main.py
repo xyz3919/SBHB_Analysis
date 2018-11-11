505917434ed1211efea1b4e44a94051af50d4168
Yu-Ching(Tony) <ycchen@illinois.edu>
2018-11-10 23:58:23 -0600

    20181110

--- python/lightcurve/main.py
+++ python/lightcurve/main.py
@@ -16,9 +16,11 @@ class lc:
         if not os.path.isdir(self.lc_dir): os.mkdir(self.lc_dir)
         self.query    = query()
         self.save_dir = self.lc_dir+save_dir
+        if not os.path.isdir(self.save_dir): os.mkdir(self.save_dir)
         self.quasar_catalog_dir = "catalog/"
         self.quasar_catalog = "spec_quasars_S1S2.txt"
         self.lc_info_file = "lc_info.csv"
+        self.log = "log"
 
         self.dtype = [("ra",float),("dec", float),("flag","|S4"),\
                       ("z",float),("where","|S6")]
@@ -42,8 +44,26 @@ class lc:
         f.write("name,ra,dec,redshift,mag_r,flag,N_g,N_r,N_i,N_z\n")
         f.close()
 
-    def create_dir(self,directory):
+    def generate_lightcurve(self,quasar):
 
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
+        band_list = ["g","r","i","z"]
+        name = self.degtohexname(quasar["ra"],quasar["dec"])
+        f = open(save_catalog,"a")
+        f.write(name+",")
+        f.write(",".join(np.array(quasar).astype(str)))
+        self.create_dir(self.save_dir+name)
+        matched_quasars = self.get_single_epoch_data(ra,dec)
+        for band in band_list:
+            print "- Fitting ",name,band
+            matched_quasars_in_band = matched_quasars[matched_quasars["BAND"]==band]
+            if len(matched_quasars_in_band) == 0 :
+                print "No data found in "+band+" !"
+                f.write(",0")
+            else:
+                f.write(","+str(len(matched_quasars_in_band)))
+                matched_quasars_in_band.to_csv(self.save_dir+name+"/"+band+".csv",\
+                                        index=False,columns=["MJD_OBS","FLUX_PSF",\
+                                        "FLUX_ERR_PSF","FLUX_AUTO","FLUX_ERR_AUTO"])
+        f.write("\n")
+        f.close()
+        return name
