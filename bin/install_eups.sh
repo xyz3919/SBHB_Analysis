074a9a627cfa68f7b22a32648d2de0aacae62b14
Yu-Ching(Tony) <ycchen@illinois.edu>
2018-12-10 01:09:40 -0600

    20181210

--- bin/install_eups.sh
+++ bin/install_eups.sh
@@ -22,6 +22,9 @@ eups distrib install astropy 1.1.2+6
 eups distrib install scipy 0.14.0+12
 eups distrib install matplotlib 1.5.3+2
 eups distrib install pandas  0.15.2+5
+eups distrib install sextractor 2.24.4+1
+eups distrib install psfex 3.21.0+5
+eups distrib install swarp 2.40.1+0
 
 # install astroquery by pip
 
@@ -36,3 +39,32 @@ $pip_used install astroquery
 $pip_used install astroML
 $pip_used install astroML_addons
 $pip_used install emcee
+
+# install fpack
+
+software_dir="software"
+
+if [ ! -e $software_dir ]
+then
+    mkdir $software_dir
+fi
+
+cd $software_dir
+mkdir bin
+
+wget http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3450.tar.gz
+
+tar xzf cfitsio3450.tar.gz
+rm  cfitsio3450.tar.gz
+
+cd cfitsio
+
+./configure --prefix=`pwd`
+make
+make install
+make fpack
+make funpack
+cp fpack ../bin/
+cp funpack ../bin/
+
+
